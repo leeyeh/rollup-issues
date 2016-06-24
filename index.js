314738be56a42456d0f12d102b02b9365f0c20e3
leeyeh <lee55962698@gmail.com>
2016-06-24 15:44:47 +0800

    rollup-plugin-commonjs@3.0.2 breaks build

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-console.log(Symbol);
-console.log(Symbol());
+console.log(Promise);
--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo ""
 echo "rollup index.js."
-echo "excute bundle.js:"
 rollup -c | tee bundle.js | node
-echo ""
-echo "rollup index.js with node_modules/** treated external."
-echo "excute bundle.external.js:"
-rollup -c rollup.config.external.js | tee bundle.external.js | node
